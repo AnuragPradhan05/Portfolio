94cc4c4427f3e183e83e25f6016a007b58abaae2
nurag-pradhan <pradhananurag2005@gmail.com>
2026-02-17 21:15:04 +0530

    fix routing production

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import About from './pages/About'
-import Projects from './pages/Projects'
-import Contact from './pages/Contact'
+import { Routes, Route, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 import { AnimatePresence } from "framer-motion";
-import { useLocation } from "react-router-dom";
 import Footer from "./components/Footer";
 
-
 function App() {
+  const location = useLocation();   
+
   return (
     <>
       <Navbar />
+
       <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
           <Route path="/" element={<Home />} />
@@ -21,9 +22,10 @@ function App() {
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </AnimatePresence>
-      <Footer />      
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
